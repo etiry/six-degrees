71e218b58ddeba28670f7c227ff0fec28205e4ae
etiry <7997357+etiry@users.noreply.github.com>
2023-08-24 16:19:13 -0400

    Add header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+// import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import promise from 'redux-promise';
+
+import Header from './containers/Header';
+// import reducers from './reducers';
+
+// const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>
 );

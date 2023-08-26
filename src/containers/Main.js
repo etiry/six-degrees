e751208c1e19cef51487726a6b3c93a5954332bd
etiry <7997357+etiry@users.noreply.github.com>
2023-08-26 09:48:02 -0400

    Move main component

---
+++ src/containers/Main.js
@@ -0,0 +1,26 @@
+import GameStatus from '../components/gameStatus/gameStatus';
+
+export default function Main () {
+  return (
+    <GameStatus />
+  )
+}
+
+
+// import axios from 'axios';
+
+// const ROOT_URL = 'https://api.tvmaze.com';
+// const PROXY_URL = 'https://corsproxy.io/?';
+
+// function Main () {
+//   const fetchPerson = (personId) => {
+//     const url = `${PROXY_URL}${ROOT_URL}/people/${personId}?embed=castcredits`;
+
+//     axios.get(url)
+//       .then((response) => console.log(response));
+//   };
+  
+//   fetchPerson(6800);
+// }
+
+// export default Main;

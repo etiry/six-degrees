88c3e44c7e6cb3e05b72b79051261f8ab5b2b28f
etiry <7997357+etiry@users.noreply.github.com>
2023-08-31 11:43:11 -0400

    Add routes

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,6 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
 import GameOverModal from '../components/gameOverModal';
 import GameStatus from '../components/gameStatus/gameStatus';
 import Options from '../components/options/options';
+import Instructions from './Instructions';
+import About from './About';
 
 /**
  * Component for displaying page child components.
@@ -9,9 +12,20 @@ import Options from '../components/options/options';
 
 const Main = () => (
   <div className="bg p-3">
-    <GameStatus />
-    <Options />
-    <GameOverModal />
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <GameStatus />
+            <Options />
+            <GameOverModal />
+          </>
+        }
+      />
+      <Route path="/how-to-play" element={<Instructions />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './app/Store';
 import Header from './containers/Header';
 import Main from './containers/Main';
@@ -10,9 +11,11 @@ import Main from './containers/Main';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Header />
-      <Main />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Header />
+        <Main />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );

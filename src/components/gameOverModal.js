d20e0124682dd169a1051f886c10ecdd6bc6b7b6
etiry <7997357+etiry@users.noreply.github.com>
2023-09-02 12:35:19 -0400

    Add detail to winning game over modal

--- src/components/gameOverModal.js
+++ src/components/gameOverModal.js
@@ -13,6 +13,14 @@ const GameOverModal = () => {
   const dispatch = useDispatch();
   const show = useSelector((state) => state.gameStatus.gameOver);
   const winner = useSelector((state) => state.gameStatus.winner);
+  const selections = useSelector((state) => state.gameStatus.selections);
+
+  const plays = selections.reduce((acc, selection) => {
+    if (selection.name) {
+      return acc + 1;
+    }
+    return acc;
+  }, -1);
 
   /**
    * When modal is closed, resets game and options components
@@ -22,18 +30,28 @@ const GameOverModal = () => {
     dispatch(resetOptions());
   };
 
-  let content = "Sorry, you're out of turns. Please try again!";
+  const createContent = () => {
+    if (winner) {
+      return (
+        <>
+          <p>You won the game!</p>
+          <p>
+            You went from {selections[0].name} to
+            {selections[selections.length - 1].name} in {plays} plays!
+          </p>
+        </>
+      );
+    }
 
-  if (winner) {
-    content = 'You won the game!';
-  }
+    return <p>Sorry, you're out of turns. Please try again!</p>;
+  };
 
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Game over!</Modal.Title>
       </Modal.Header>
-      <Modal.Body>{content}</Modal.Body>
+      <Modal.Body>{createContent()}</Modal.Body>
       <Modal.Footer>
         <Button variant="custom" className="custom-btn" onClick={handleClose}>
           Close

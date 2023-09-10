b1a9f3ccdfc9cb8a4bac777cb7cd6dc257658023
etiry <7997357+etiry@users.noreply.github.com>
2023-09-10 13:50:04 -0400

    Add collapsible header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,6 @@ import { LinkContainer } from 'react-router-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Button from 'react-bootstrap/Button';
 import defaultStartingOptions from '../assets/defaultStartingOptions.json';
 import {
   incrementCurrentDegree,
@@ -59,54 +58,96 @@ const Header = () => {
 
   return (
     <>
-      <Navbar className="bg-body-tertiary">
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
         <Container>
           <LinkContainer to="/">
-            <Navbar.Brand>Six Degrees of TV</Navbar.Brand>
+            <Navbar.Brand href="#home">Six Degrees of TV</Navbar.Brand>
           </LinkContainer>
-          <div>
-            <LinkContainer to="/">
-              <Button
-                variant="custom"
-                className="custom-btn"
-                onClick={handleStartGameClick}
-                disabled={gameInProgress}
-              >
-                Start game
-              </Button>
-            </LinkContainer>
-            <LinkContainer to="/">
-              <Button
-                variant="custom"
-                className="custom-btn"
-                onClick={handleResetClick}
-              >
-                Reset game
-              </Button>
-            </LinkContainer>
-            <LinkContainer to="/">
-              <Button
-                variant="custom"
-                className="custom-btn"
-                onClick={handleChangeTargetClick}
-              >
-                Change target actor
-              </Button>
-            </LinkContainer>
-          </div>
-          <div className="d-flex">
-            <LinkContainer to="/how-to-play">
-              <Nav.Link className="p-3">How to Play</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Nav.Link className="p-3">About</Nav.Link>
-            </LinkContainer>
-          </div>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link
+                  onClick={handleStartGameClick}
+                  disabled={gameInProgress}
+                >
+                  Start Game
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/">
+                <Nav.Link onClick={handleResetClick}>Reset Game</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/">
+                <Nav.Link onClick={handleChangeTargetClick}>
+                  Change Target Actor
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+            <Nav>
+              <LinkContainer to="/how-to-play">
+                <Nav.Link>How to Play</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
       <ChangeTargetModal showModal={showModal} setShowModal={setShowModal} />
     </>
   );
+
+  // return (
+  //   <>
+  //     <Navbar className="bg-body-tertiary">
+  //       <Container>
+  //         <LinkContainer to="/">
+  //           <Navbar.Brand>Six Degrees of TV</Navbar.Brand>
+  //         </LinkContainer>
+  //         <div>
+  //           <LinkContainer to="/">
+  //             <Button
+  //               variant="custom"
+  //               className="custom-btn"
+  //               onClick={handleStartGameClick}
+  //               disabled={gameInProgress}
+  //             >
+  //               Start game
+  //             </Button>
+  //           </LinkContainer>
+  //           <LinkContainer to="/">
+  //             <Button
+  //               variant="custom"
+  //               className="custom-btn"
+  //               onClick={handleResetClick}
+  //             >
+  //               Reset game
+  //             </Button>
+  //           </LinkContainer>
+  //           <LinkContainer to="/">
+  //             <Button
+  //               variant="custom"
+  //               className="custom-btn"
+  //               onClick={handleChangeTargetClick}
+  //             >
+  //               Change target actor
+  //             </Button>
+  //           </LinkContainer>
+  //         </div>
+  //         <div className="d-flex">
+  //           <LinkContainer to="/how-to-play">
+  //             <Nav.Link className="p-3">How to Play</Nav.Link>
+  //           </LinkContainer>
+  //           <LinkContainer to="/about">
+  //             <Nav.Link className="p-3">About</Nav.Link>
+  //           </LinkContainer>
+  //         </div>
+  //       </Container>
+  //     </Navbar>
+  //     <ChangeTargetModal showModal={showModal} setShowModal={setShowModal} />
+  //   </>
+  // );
 };
 
 export default Header;
